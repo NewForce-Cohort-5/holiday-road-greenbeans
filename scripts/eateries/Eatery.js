62c30bb5203709b46e0998008145605bca46040c
heavenburdette1988 <heavenburdette1988@gmail.com>
2021-11-03 11:00:17 -0400

    added button with bootstrap

--- scripts/eateries/Eatery.js
+++ scripts/eateries/Eatery.js
@@ -1,7 +1,7 @@
 export const Eatery = (eateries)=> {
     return `
     <section class="eatery-card">
-    <button type="button" class="btn btn-dark btn-outline-light ">Deatils</button>
+    <button type="button" class="btn btn-dark btn-outline-light" id="detailButton">Details</button>
     <h3 class="business__name"> ${eateries.businessName}</h3>
     <p class="business__location"> ${eateries.city}, ${eateries.state}</p>
     </section>
--- scripts/parks/Park.js
+++ scripts/parks/Park.js
@@ -1,8 +1,9 @@
 export const park = (parkObject) => {
     return`
     <section class="parkCard">
+    <button type="button" class="btn btn-dark btn-outline-light ">Details</button>
     <h3 class="fullName">${parkObject.fullName}</h3>
-    <button class="Details">Details</button>
+
     </section>`
 }
 
