f1ea8cc84657074d9af71b36acecf0ab6c53ddc0
heavenburdette1988 <heavenburdette1988@gmail.com>
2021-11-04 10:17:13 -0400

    working on printing details button

--- scripts/parks/Park.js
+++ scripts/parks/Park.js
@@ -1,4 +1,4 @@
-export const park = (parkObject) => {
+export const park = (parkObject, parkObject2) => {
     return`
     <section class="parkCard">
     <button type="button" id="detailButtonPark" class="btn btn-dark btn-outline-light">Details</button>
@@ -7,10 +7,13 @@ export const park = (parkObject) => {
     <div class="hidden"
     id="parkDetailButton"><h3>Park Details</h3>
     <div class="description">Description: ${parkObject.description}</div>
-    <div class="activities">Activities: ${parkObject.activities}</div>
-    <div class="contacts">Contact: ${parkObject.contacts.phoneNumbers}</div>
+    <div class="description">Contacts: ${parkObject.contacts.phoneNumber}</div>
+    
+    
+
     </div>
     </section>`
 }
 
 //need to add additional info- just using this to print
+// ${eateries.ameneties.restrooms ? `<li>Restrooms</li>` : ""}
